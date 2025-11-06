9449c80a48408969f0f42d1977de7cbabcb7ae01
andygildev <nafaghagilbert@gmail.com>
2025-11-06 06:28:56 -0800

    prep 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Runtime.ConstrainedExecution;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1, 101);
+
+
+        int guess = 0;
+        
+        while (guess != magic)
+        {
+            Console.Write("what is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+
+            if (magic < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else if (magic > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }    
     }
 }
